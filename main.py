c6ae331050e47581bb3fa7009c72949eaec25d40
abraham05 <abrahamgue05@gmail.com>
2024-12-06 17:19:54 -0600

    Cambios al main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
+import time # Se importa la libreria time para dar descansos entre una funcion y otra
 
-user = input('Dame tu nombre: ')
+user = input('Dame tu nombre: ') # Se le solicita un dato al usuario
 
-print(user)
+print(user) # Imprimimos el dato del usuario
