965469a3c4c80438f839d92856156cb6d31cb8e7
José Esteva <zbrmr34@gmail.com>
2024-12-06 17:56:15 -0600

    feat: Barra de carga

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # Se importa la librería time para dar descansos entre una función y otra
 import time
 
+def linea_espera(time_sec):
+  for i in range(0,time_sec):
+    print('-', end='')
+    time.sleep(1)
+  print('')
+
 # Se le solicita un dato al usuario
 user = input('Dame tu nombre: ')
+
+# Implementamos una linea de carga
+print('Procesando informacion')
+linea_espera(5)
 
 # Imprimimos el dato del usuario
 print(user)
 
-time.sleep(2)
+
+
